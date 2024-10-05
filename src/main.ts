c5aea0a4a9648c0c60c0822ba94d1d4d66c52b06
Julius Truksinas <julius@ababa.tech>
2024-10-06 02:52:39 +0300

    new: sukuriau default.yml faila, is kurio bus imamos default konfiguracijos (MIFT-51)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import config from 'config';
 import mongoose from 'mongoose';
 import app from './app';
 import logger from './logger';
 
-const PORT = process.env.PORT ?? 3000;
-const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/db';
+const serverConfig: any = config.get('server');
+const dbConfig: any = config.get('db');
+
+const PORT = process.env.PORT ?? serverConfig.port;
+const MONGO_URI = process.env.MONGO_URI ?? dbConfig.mongoUri;
 
 async function bootstap() {
   try {
-    await mongoose.connect(
-      MONGO_URI.replace('<DB_PASSWORD>', process.env.DB_PASSWORD as string),
-    );
+    await mongoose.connect(MONGO_URI);
   } catch (e) {
     logger.error('Failed to connect to the DB!');
   }
